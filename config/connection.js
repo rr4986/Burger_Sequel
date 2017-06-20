5891624176f603da89d79c564c8bc45670c5fddc
Ruben Reyes <ruben.r.reyes@gmail.com>
2017-06-19 20:27:30 -0500

    new changes

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
 var mysql = require("mysql");
+var connection;
 
-  
-});
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_UR);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "yaha9012",
     database: "burgers_db"
-  })
-}
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.log("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
