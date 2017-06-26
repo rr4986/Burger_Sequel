191ab3ef15c9b88d4d0d2e5f681ec5f1fcb509ac
Ruben Reyes <ruben.r.reyes@gmail.com>
2017-06-26 18:33:04 -0500

    removed passwords

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "yaha9012",
+    password: "",
     database: "burgers_db"
   });
 };
